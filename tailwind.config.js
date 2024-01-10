e7344f080f189150fd0489aefd1327a68fab6481
Munshi91 <mimunshi91@gmail.com>
2024-01-10 23:32:35 +0600

    html file path change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: ["../dist/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
